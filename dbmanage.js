2ef605e91852c2cb95675df5b713911bd373ff2b
tonyxforce <horich.dominik@gmail.com>
2023-10-30 09:53:47 +0100

    migrate to typescript

--- dbmanage.js
+++
@@ -1,9 +0,0 @@
-/* var fs = require('fs');
-
-exports.set = (_, db) => {
-    fs.writeFileSync("db.json", db);
-}
-
-exports.get = () => {
-    return JSON.parse(fs.readFileSync("db.json"));
-}; */
--- handlers/ready.js
+++
@@ -1,5 +0,0 @@
-module.exports = (bot)=>{
-    bot.on("ready", ()=>{
-        console.log(`${bot.user.tag} started!`)
-    })
-}
